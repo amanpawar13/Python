49a002a3dba932b0adfa312d5dd61de46addfb7c
amanpawar13 <aman.pawar1@outlook.com>
2025-05-25 16:58:29 +0530

    Create task_1.py
    1st task of calculations based on inout values.

---
+++ task_1.py
@@ -0,0 +1,14 @@
+a = input("Enter the first number: ")
+b = input("Enter the Second number: ")
+a=float(a)
+b=float(b)
+
+a1= a+b
+s= a-b
+m = a*b
+d = a/b
+
+print("Addition :",a1)
+print("Subtraction :",s)
+print("Multiplication :",m)
+print("Division :",d)
